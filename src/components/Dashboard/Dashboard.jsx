be8f25d9ab68da94c3eec5f6de7b34ce7a30f84f
Soraiya11-7 <soraiyaparvin@gmail.com>
2024-11-02 19:31:01 +0600

    Navbar and Footer is added

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2 className="text-6xl">Dashboard</h2>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text-6xl text-bold">Not Found!</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
